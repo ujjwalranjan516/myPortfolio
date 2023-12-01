c18220d8dc2d92e15c2064e1381d9bee91e6d0c5
ujjwalranjan516 <82654014+ujjwalranjan516@users.noreply.github.com>
2023-12-01 23:50:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+  var tablinks=document.getElementsByClassName("tab-links");
+  var tabcontents=document.getElementsByClassName("tab-contents");
+
+  function opentab(tabname){
+    for(tablink of tablinks) {
+      
+    }
+   
+    
+  }
